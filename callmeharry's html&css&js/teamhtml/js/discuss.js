b1986ee3a22dac207a8c6fc1b7546ad54d5f1730
callmeharry <cmhsteveli@gmail.com>
2014-09-20 21:20:48 +0800

    discuss更新

--- callmeharry's html&css&js/teamhtml/js/discuss.js
+++ callmeharry's html&css&js/teamhtml/js/discuss.js
@@ -1,4 +1,4 @@
-// JavaScript Document
+﻿// JavaScript Document
 function init(uid)
 {
 	function do_verify() {
@@ -36,6 +36,17 @@ function show_new_reply(a){
 	var array = new Array();
 	array = a.name.split("-")
 	var mid = array[4];
+	var id = "news-reply-new-main-" + mid;
+	document.getElementById(id).style.display = "block";
+	var hide_id = "news-reply-new-tab-div-" + mid;
+	document.getElementById(hide_id).style.display = "none";
+}
+function reply_adduser(a){
+	var array = new Array();
+	array = a.name.split("-");
+	var name = array[0];
+	var id = array[1];
+	document.getElementById("news-reply-new-input-"+id).innerText="回复" + name;
 	var id = "news-reply-new-main-" + mid;
 	document.getElementById(id).style.display = "block";
 	var hide_id = "news-reply-new-tab-div-" + mid;
